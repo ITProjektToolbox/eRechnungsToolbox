b6fbd4d368c5f2942eaaa71047b5f6cfd99ea5c1
ITProjektToolbox <62174276+ITProjektToolbox@users.noreply.github.com>
2020-03-14 20:10:32 +0100

    Add files via upload

---
+++ XRechnungsdrucker/PipeClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+
+namespace PipeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string logFilePath = @"C:\XRechnungsDrucker\logDebug.txt";
+
+            using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "XRechnungsDruckerPipe", PipeDirection.Out))
+            {
+                try
+                {
+                    var inputLines = new List<string>();
+                    string s;
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(logFilePath))
+                    {
+                        while (null != (s = Console.ReadLine()))
+                        {
+                            inputLines.Add(s);
+                            file.WriteLine(s);
+                        }
+                    }
+                    pipeClient.Connect(3000);
+
+                    using (StreamWriter sr = new StreamWriter(pipeClient))
+                    {
+                        foreach (var line in inputLines)
+                        {
+                            sr.WriteLine(line);
+                        }
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(logFilePath))
+                    {
+                        file.WriteLine(e.Message);
+                    }
+                }
+            }
+        }
+    }
+}
+
