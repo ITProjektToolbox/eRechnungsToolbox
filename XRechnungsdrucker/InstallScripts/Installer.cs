90f0d631fa21387d8714f58d9c5705793622bcb9
ITProjektToolbox <62174276+ITProjektToolbox@users.noreply.github.com>
2020-03-14 20:11:15 +0100

    Add files via upload

---
+++ XRechnungsdrucker/InstallScripts/Installer.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace XRechnungsDruckerSetupCustomAction
+{
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        public Installer()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            string printerName = "XRechnungsDrucker";
+            base.Install(stateSaver);
+            LogHelper.Log("Install Started.");
+
+                try
+                {
+                    SpoolerHelper sh = new SpoolerHelper();
+                    SpoolerHelper.GenericResult result = sh.AddVPrinter(printerName, printerName);
+                    if (result.Success == false)
+                    {
+                        LogError(result.Method, result.Message, result.Exception);
+                        throw new InstallException(string.Format("Source: {0}\nMessage: {1}", result.Method, result.Message), result.Exception);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogError("AddVPrinter", ex.Message, ex);
+                }
+            LogHelper.Log("Install Finished.");
+        }
+
+        private static void LogError(string exceptionSource, string message, Exception innerException)
+        {
+            string eventMessage = string.Format("Source: {0}\nMessage: {1}\nInnerException: {2}", exceptionSource, message, innerException);
+            LogHelper.Log(eventMessage);
+        }
+    }
+}
---
+++ XRechnungsdrucker/InstallScripts/LogHelper.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+namespace XRechnungsDruckerSetupCustomAction
+{
+    public static class LogHelper
+    {
+        public static void Log(string msg)
+        {
+            var filename = "C:\\XRechnungsDrucker_Installer.txt";
+            using (var sw = new System.IO.StreamWriter(filename, true))
+            {
+                sw.Write(string.Format("{0} - {1}\n", DateTime.Now, msg));
+                sw.Flush();
+            }
+        }
+    }
+}
